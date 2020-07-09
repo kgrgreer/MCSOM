3d1c9e392502ee78594745b845d08cccb8a55933
Kevin Greer <kgrgreer@gmail.com>
2020-07-09 17:11:44 -0400

    Simplified.

--- src/test.c
+++ src/test.c
@@ -4,23 +4,23 @@
 // Start of Simple Object System
 
 #define LOOKUP(o, m)                ((Class) ((o)->class))(m)
-#define CALL(o, m)                  (LOOKUP(o, m))((SObject*) (o))
-#define CALL1(o, m, a1)             (((Method1) LOOKUP(o, m)))((SObject*) (o), (SObject*) (a1))
-#define CALL2(o, m, a1, a2)         (((Method2) LOOKUP(o, m)))((SObject*) (o), (SObject*) (a1), (SObject*) (a2))
-#define CALL3(o, m, a1, a2, a3)     (((Method3) LOOKUP(o, m)))((SObject*) (o), (SObject*) (a1), (SObject*) (a2), (SObject*) (a3))
-#define CALL4(o, m, a1, a2, a3, a4) (((Method4) LOOKUP(o, m)))((SObject*) (o), (SObject*) (a1), (SObject*) (a2), (SObject*) (a3), (SObject*) (a4))
+#define CALL(o, m)                  (LOOKUP(o, m))((SObject) (o))
+#define CALL1(o, m, a1)             (((Method1) LOOKUP(o, m)))((SObject) (o), (SObject) (a1))
+#define CALL2(o, m, a1, a2)         (((Method2) LOOKUP(o, m)))((SObject) (o), (SObject) (a1), (SObject) (a2))
+#define CALL3(o, m, a1, a2, a3)     (((Method3) LOOKUP(o, m)))((SObject) (o), (SObject) (a1), (SObject) (a2), (SObject) (a3))
+#define CALL4(o, m, a1, a2, a3, a4) (((Method4) LOOKUP(o, m)))((SObject) (o), (SObject) (a1), (SObject) (a2), (SObject) (a3), (SObject) (a4))
 
 typedef short MethodId;
-typedef struct SObject_ SObject;
-typedef SObject* (*Method)(SObject*);
-typedef SObject* (*Method1)(SObject*, SObject*);
-typedef SObject* (*Method2)(SObject*, SObject*, SObject*);
-typedef SObject* (*Method3)(SObject*, SObject*, SObject*, SObject*);
-typedef SObject* (*Method4)(SObject*, SObject*, SObject*, SObject*, SObject*);
+typedef struct SObject_* SObject;
+typedef SObject (*Method)(SObject);
+typedef SObject (*Method1)(SObject, SObject);
+typedef SObject (*Method2)(SObject, SObject, SObject);
+typedef SObject (*Method3)(SObject, SObject, SObject, SObject);
+typedef SObject (*Method4)(SObject, SObject, SObject, SObject, SObject);
 typedef Method   (*Class)(MethodId);
 struct SObject_ { Class *class; };
 
-SObject* no_such_method(SObject *this) { printf("no such method\n"); return this; }
+SObject no_such_method(SObject this) { printf("no such method\n"); return this; }
 
 // End of Simple Object System
 
@@ -34,12 +34,12 @@ SObject* no_such_method(SObject *this) { printf("no such method\n"); return this
 #define NAME    6
 
 
-SObject* f1(SObject *this) {
+SObject f1(SObject this) {
   printf("f1()\n");
   return this;
 }
 
-SObject* f2(SObject *this) {
+SObject f2(SObject this) {
   printf("f2()\n");
   return this;
 }
@@ -56,18 +56,18 @@ typedef struct SInteger_* SInteger;
 
 SInteger newSInteger(int i);
 
-SObject* SInteger_name(SObject *this) {
+SObject SInteger_name(SObject this) {
   printf("SInteger\n");
   return this;
 }
 
-SObject* SInteger_println(SObject *this) {
+SObject SInteger_println(SObject this) {
   printf("%d\n", ((SInteger) this)->value);
   return this;
 }
 
-SObject* SInteger_plus(SObject *this, SObject *that) {
-  return (SObject *) newSInteger(((SInteger) this)->value + ((SInteger) that)->value);
+SObject SInteger_plus(SObject this, SObject that) {
+  return (SObject) newSInteger(((SInteger) this)->value + ((SInteger) that)->value);
 }
 
 // "Class" Test
