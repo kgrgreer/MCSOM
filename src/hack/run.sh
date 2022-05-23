0d42af8c16a2f2cb1d8e26b38d87846e638b0f14
Kevin Greer <kgrgreer@gmail.com>
2022-05-23 17:10:43 -0400

    Assembler: Add for loop macro.

--- src/hack/run.sh
+++ src/hack/run.sh
@@ -2,3 +2,5 @@ echo 'Prog1'
 ~/SOM/som-java/som.sh -cp ~/SOM/som-java/Smalltalk Prog1.som
 echo 'Prog1M'
 ~/SOM/som-java/som.sh -cp ~/SOM/som-java/Smalltalk Prog1M.som
+echo 'Prog2M'
+~/SOM/som-java/som.sh -cp ~/SOM/som-java/Smalltalk Prog2M.som
